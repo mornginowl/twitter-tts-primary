88b0a71f926f9068ee42bcb1926aef9d22a71165
unknown <salazar.carlosno9@gmail.com>
2019-08-27 09:15:29 -0500

    added devise, bootstrap and carrier wave and set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with :exception
+	protect_from_forgery with: :exception
 
 	before_action :configure_permitted_parameter, if: :devise_controller?
 	protected
 	def configure_permitted_parameter
-		devise_parameter_sanitizer.permit(:sign_up, keys:[:username,:name,:bio,:location])
-		devise_parameter_sanitizer.permit(:account_update, keys:[:username,:name,:bio,:location])
+		devise_parameter_sanitizer.permit(:sign_up, keys:[:username,:name,:bio,:location, :avatar])
+		devise_parameter_sanitizer.permit(:account_update, keys:[:username,:name,:bio,:location, :avatar])
 	end
 end
